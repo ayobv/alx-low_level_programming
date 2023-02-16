596215ea4e986884247de15540aabc78a3377c23
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-16 15:20:21 -0800

    jybsjn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/* first C program */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
-
