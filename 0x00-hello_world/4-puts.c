981644756654dbb196f05da0418b309604f458e7
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-16 15:26:42 -0800

    jdkdh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-/* first C program */
+/**
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
