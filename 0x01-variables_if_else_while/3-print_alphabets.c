cbbd12f6868df38b941654aef5f5bd1342faf657
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-17 14:06:45 -0800

    jhj

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main(void)
 {
 int i;
 for (i = 97; i < 123; i++)
-        putchar(i);
+	putchar(i);
 for (i = 65; i < 91; i++)
-        putchar(i);
+	putchar(i);
 putchar('\n');
 return (0);
 }
