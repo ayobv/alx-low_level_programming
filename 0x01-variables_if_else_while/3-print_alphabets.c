02af2454d8217b008d18a7bf2bbf3b1496e8aed3
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-17 14:02:32 -0800

    higher

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints alphabet in lower and higher cases
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+        putchar(i);
+for (i = 65; i < 91; i++)
+        putchar(i);
+putchar('\n');
+return (0);
+}
