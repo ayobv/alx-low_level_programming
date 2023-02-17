895211a783fb5e9be1108398e41f63d843a8e168
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-17 14:51:28 -0800

    gsvdj

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints alphabet in reverse order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 122; i >= 97; i--)
+        putchar(i);
+putchar('\n');
+return (0);
+}
