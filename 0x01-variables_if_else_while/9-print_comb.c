25e4fe06002d20e900787a9e6d85f7e9703979be
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-17 15:41:08 -0800

    hdusdh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints all combination of single digit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+	for (b = 0; b < 10; b++)
+	{
+		putchar(b + '0');
+		if (b < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
