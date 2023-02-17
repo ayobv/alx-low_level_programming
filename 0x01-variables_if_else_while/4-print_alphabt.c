2031c96ae340481b8d1496c91542846e09c7134e
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-17 14:20:40 -0800

    jasg

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints alphabet in lower cases except q,e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+	if (i != 101 && i != 113)
+        putchar(i);
+putchar('\n');
+return (0);
+}
