29e9b91649361a21078b90c9cdddff9670d2e5eb
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 12:35:44 -0800

    betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - to print void
+ *
+ * This function prints alphabet 10 times
+ *
+ * Return: Always 0 (Success)
+ */
 
 void print_alphabet_x10(void)
 {
