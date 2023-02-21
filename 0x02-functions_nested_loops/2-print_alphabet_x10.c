aa4245de8c6740877fa8072b0c5525ab08338476
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 12:25:00 -0800

    skh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,10 +3,10 @@
 void print_alphabet_x10(void)
 {
 	char letter;
-       	int i;
+	int i;
 	
-	for (i = 0; i < 10; i++)
-	{
+	for (i = 0; i < 10; i++){
+
 		letter = 'a';
 		while (letter <= 'z')
 		{
