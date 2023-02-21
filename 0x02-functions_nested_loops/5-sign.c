37bfc0edbd11be6b3c13815560969a1bb2a860c3
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 14:56:49 -0800

    jddb

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,6 +4,7 @@
 /**
  * print_sign - check the code
  *
+ * @n: any int number
  * Return: Always 1 oe 0 or -1.
  */
 int print_sign(int n)
@@ -11,17 +12,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return 1;
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return 0;
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		return -1;
+		return (-1);
 	}
 }
 
