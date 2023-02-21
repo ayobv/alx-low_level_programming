91e519f6eef148b4c6a8ae04d999e19db3955f46
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 14:14:25 -0800

    hdsk

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _islower - Write a function that checks for lowercase character.
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,7 @@ int _putchar(char c);
 void print_alphabet(void);
 /* print_alphabet_x10 prototype */
 void print_alphabet_x10(void);
+/* print_is lower */
+int _islower(int c);
 #endif /* MAIN_H */
 
