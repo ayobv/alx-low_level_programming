174b3f1db21bb951e5dfa365ae2f0908db086d72
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 10:07:19 -0800

    editing betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,21 +6,23 @@
  *
  * This function prints the '_putchar'
  *
+ * _putchar - writes character
+ *
  * Return: Always 0 (Success)
  */
 
 int _putchar(char c);
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
 
-    return (0);
+return (0);
 }
