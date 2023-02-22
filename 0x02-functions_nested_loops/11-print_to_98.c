f84576eca4851b5b055b32f391e5993874def243
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 17:24:21 -0800

    sjnhan

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,19 @@
  * Return: Always absolute value of last digit
  */
 
-void print_to_98(int n) {
-    if (n > 98) {
-        for (int i = n; i >= 98; i--) {
+void print_to_98(int n)
+{
+    int i;
+    
+    if (n <= 98) {
+        for (i = n; i <= 98; i++) {
             printf("%d", i);
             if (i != 98) {
                 printf(", ");
             }
         }
     } else {
-        for (int i = n; i <= 98; i++) {
+        for (i = n; i >= 98; i--) {
             printf("%d", i);
             if (i != 98) {
                 printf(", ");
