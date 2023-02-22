17d9a871fc00f137d8ea236def80d2c7018f079f
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 17:13:41 -0800

    hdvsj

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * add - sums two numbers
+ *
+ * This function adds two numbers
+ *
+ * Return: sum of two numbers
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
