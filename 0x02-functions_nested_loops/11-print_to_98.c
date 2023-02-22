bfe80dd446032522a98b59acaa62935bf2177f80
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 17:31:55 -0800

    nbb

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,34 +4,42 @@
 #include <stdlib.h>
 
 /**
- * print_to_98 - prints last digit
+ * print_to_98 - prints to 98
  *
- * This function prints last digits
+ * This function prints to 98
  *
- * @num: is any integer
+ * @n: is any integer
+ *
+ * @i: is any integer
  *
  * Return: Always absolute value of last digit
  */
 
 void print_to_98(int n)
 {
-    int i;
-    
-    if (n <= 98) {
-        for (i = n; i <= 98; i++) {
-            printf("%d", i);
-            if (i != 98) {
-                printf(", ");
-            }
-        }
-    } else {
-        for (i = n; i >= 98; i--) {
-            printf("%d", i);
-            if (i != 98) {
-                printf(", ");
-            }
-        }
-    }
-    printf("\n");
+	int i;
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
 }
 
