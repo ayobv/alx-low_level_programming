093f5261426af3c6bd6669e7e8e11dc3f9970418
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 16:57:51 -0800

    dsbh

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,32 +14,38 @@
  */
 void times_table(void)
 {
-	int i, j, result;
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			result = i * j;
-			if (result > 9)
-			{
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
-			}
-			else
-			{
-				_putchar(result + '0');
-			}
-			if (j == 9)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-		}
-	}
+    int i, j, result;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
+
+            if (j == 0)
+            {
+                /* print the first column with no commas */
+                _putchar('0' + result);
+            }
+            else
+            {
+                /* print the other columns with commas and spaces */
+                _putchar(',');
+                _putchar(' ');
+
+                if (result < 10)
+                {
+                    /* add an extra space for single-digit numbers */
+                    _putchar(' ');
+                }
+
+                _putchar('0' + result);
+            }
+        }
+
+        /* print a newline at the end of each row */
+        _putchar('\n');
+    }
 }
+
 
