8211d62e5e96641df0a40551f378b8b5eafd8062
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 16:43:13 -0800

    jdhsn

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,6 @@
 void times_table(void)
 {
 	int i, j, result;
-	
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
@@ -23,7 +22,7 @@ void times_table(void)
 			result = i * j;
 			if (result > 9)
 			{
-			       	_putchar(result / 10 + '0');
+				_putchar(result / 10 + '0');
 				_putchar(result % 10 + '0');
 			}
 			else
@@ -36,8 +35,8 @@ void times_table(void)
 			}
 			else
 			{
-			       	_putchar(',');
-				_putchar(' ');
+				_putchar(',');
+				_putchar('  ');
 			}
 		}
 	}
