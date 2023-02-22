a0a197bcf7d3b6944f63eef6c1cbe3d5f72db05c
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-21 16:36:56 -0800

    shdbxb

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * times_table - prints nothing
+ *
+ * This function prints 9 times table
+ *
+ * _putchar - prints character
+ *
+ * Return: Always a value
+ */
+void times_table(void)
+{
+	int i, j, result;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (result > 9)
+			{
+			       	_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(result + '0');
+			}
+			if (j == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+			       	_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
+
