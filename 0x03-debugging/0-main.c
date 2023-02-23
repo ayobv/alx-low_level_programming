1a61cde42c8f4803d9fd1d55c4a550c5b41e97e1
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-23 07:30:47 -0800

    dhf

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ *
+ * @i: Any integer but defind here as 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
+
