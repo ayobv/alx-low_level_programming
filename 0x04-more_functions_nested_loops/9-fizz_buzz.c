c4fc5cd8463bb14b172433387d53e66f9fbb5fb1
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-23 21:11:38 -0800

    jdj

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,73 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * fizz_buzz - Prints the numbers from 1 to 100, replacing
- * multiples of 3 with Fizz, multiples of 5 with Buzz,
- * and multiples of both with FizzBuzz.
- */
-void fizz_buzz(void)
+int main(void)
 {
     int i;
-
-    for (i = 1; i <= 100; i++)
-    {
-        if ((i % 3 == 0) && (i % 5 == 0))
-        {
-            _putchar('F');
-            _putchar('i');
-            _putchar('z');
-            _putchar('z');
-            _putchar('B');
-            _putchar('u');
-            _putchar('z');
-            _putchar('z');
-        }
-        else if (i % 3 == 0)
-        {
-            _putchar('F');
-            _putchar('i');
-            _putchar('z');
-            _putchar('z');
-        }
-        else if (i % 5 == 0)
-        {
-            _putchar('B');
-            _putchar('u');
-            _putchar('z');
-            _putchar('z');
-        }
-        else
-        {
-            int num = i;
-            int digits = 0;
-
-            while (num != 0)
-            {
-                digits++;
-                num /= 10;
-            }
-
-            num = i;
-
-            while (num != 0)
-            {
-                int digit = num % 10;
-                _putchar(digit + '0');
-                num /= 10;
-            }
-
-            while (digits < 2)
-            {
-                _putchar(' ');
-                digits++;
-            }
-        }
-
-        if (i != 100)
-        {
-            _putchar(' ');
+    for (i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz ");
+        } else if (i % 3 == 0) {
+            printf("Fizz ");
+        } else if (i % 5 == 0) {
+            printf("Buzz ");
+        } else {
+            printf("%d ", i);
         }
     }
-
-    _putchar('\n');
-}
+    printf("\n");
+    return 0;
+}   
