177e6a6021c1078e25d0e009f8f305583f6de985
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-23 19:55:35 -0800

    ahja

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _isdigit - check code
+ *
+ * return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return 1;
+	else
+		return (0);
+}
