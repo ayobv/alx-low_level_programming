9c9e426d9a524e2892aabaa589bc9c15f15f5c28
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-02-27 14:17:56 -0800

    sjd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,37 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first
- * character, followed by a new line
- * @str: pointer to the string to print
+ * _strlen - returns the length of a string
+ * @s: string to get length of
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string to print
  */
 void puts_half(char *str)
 {
-	int i;
+	int len = _strlen(str);
+	int i, start;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-	}
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len - 1) / 2 + 1;
+
+	for (i = start; i < len; i++)
+		_putchar(*(str + i));
+
 	_putchar('\n');
 }
