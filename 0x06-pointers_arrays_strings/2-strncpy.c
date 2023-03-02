f3a62d005fca9f553ba7e972a9940f435fb8a536
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-03-02 11:45:13 -0800

    sjdh

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+/**
+ * _strncpy - Copy a string
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ * @n: Maximum number of characters to copy
+ *
+ * Return: Pointer to the destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
