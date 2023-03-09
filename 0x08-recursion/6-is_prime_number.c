d2cb910a3ac18f7bf90346844ad86020f45f794a
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-03-08 16:25:07 -0800

    ahk

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,10 +17,19 @@ int is_prime_helper(int n, int i)
 		return (1);
 	if (n % i == 0)
 		return (0);
-	return is_prime_helper(n, i + 1);
+	return (is_prime_helper(n, i + 1));
 }
 
+/**
+ * is_prime_helper - helper function to check if a given integer is prime
+ *
+ * is_prime_number - see function for details
+ *
+ * @n: the integer to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
 int is_prime_number(int n)
 {
-	return is_prime_helper(n, 2);
+	return (is_prime_helper(n, 2));
 }
