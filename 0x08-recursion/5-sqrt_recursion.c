5bda1f4cea224e287a5b7d6770ef515cbc4db06e
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-03-08 17:00:32 -0800

    ajda

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,16 +4,16 @@
  * _sqrt_recursion - returns the natural square root of a number
  * @n: the number to find the square root of
  *
- * Return: the natural square root of n, or -1 if n does not have a natural square root
+ * Return: the natural square root of n, or -1 if n does not have
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	else if (n == 0 || n == 1)
-		return n;
+		return (n);
 	else
-		return _sqrt_helper(n, 1, n);
+		return (_sqrt_helper(n, 1, n));
 }
 /**
  * _sqrt_helper - helper function for _sqrt_recursion
@@ -21,18 +21,18 @@ int _sqrt_recursion(int n)
  * @start: the starting value for the search
  * @end: the ending value for the search
  *
- * Return: the natural square root of n, or -1 if n does not have a natural square root
+ * Return: the natural square root of n, or -1 if n does not
  */
 int _sqrt_helper(int n, int start, int end)
 {
 	int mid = (start + end) / 2;
 
 	if (start > end)
-		return -1;
+		return (-1);
 	else if (mid * mid == n)
-		return mid;
+		return (mid);
 	else if (mid * mid > n)
-		return _sqrt_helper(n, start, mid - 1);
+		return (_sqrt_helper(n, start, mid - 1));
 	else
-		return _sqrt_helper(n, mid + 1, end);
+		return _sqrt_helper((n, mid + 1, end));
 }
