c76373101361ce7c457aed112a634abf60d28a4f
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-03-08 16:55:58 -0800

    jsdbsj

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,7 +2,7 @@
 
 /**
  * wildcmp - compares two strings and returns a value
- * 
+ *
  * @s1: pointer to first string
  * @s2: pointer to second string
  *
@@ -11,19 +11,19 @@
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-		return 1;
+		return (1);
 
 	if (*s1 == *s2)
-		return wildcmp(s1 + 1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
 	{
 		if (wildcmp(s1, s2 + 1))
-			return 1;
+			return (1);
 
 		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
-			return 1;
+			return (1);
 	}
-	return 0;
+	return (0);
 }
 
