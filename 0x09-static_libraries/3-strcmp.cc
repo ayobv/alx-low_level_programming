c8e41eb5c5edbc91a25aae83851e17974728de3f
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-03-10 19:25:24 -0800

    hdh

---
+++ 0x09-static_libraries/3-strcmp.cc
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: the first string to compare
+ * @s2: the second string to compare
+ *
+ * Return: an integer less than, equal to, or greater than zero
+ * depending on the comparison result
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (0);
+}
