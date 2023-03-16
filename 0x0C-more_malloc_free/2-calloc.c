06f5241a4f8eda852c90cfb5edcec513e55704fd
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-03-16 13:30:16 -0700

    shdjs

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef>
+
+/**
+ * _calloc - Allocates memory for an array using malloc
+ * @nmemb: Number of elements to allocate
+ * @size: Size of each element in bytes
+ *
+ * Return: A void pointer to the allocated memory or NULL on failure
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
+
+	return ((void *)ptr);
+}
