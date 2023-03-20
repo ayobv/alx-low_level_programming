1fe22209e4682c5360ed27963b7b8625cfdcc9d2
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-03-20 14:39:14 -0700

    using struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - a structure representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner of the dog
+ *
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
