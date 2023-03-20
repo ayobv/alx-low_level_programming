a97c27d56243d4077e993dc47d1c0254e1846702
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-03-20 13:56:53 -0700

    prints file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("This program was compiled from the file: %s\n", __FILE__);
+	return (0);
+}
