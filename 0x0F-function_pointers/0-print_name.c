e1d8b3d2bff9094ec14d2f0f618366a6d10f701a
Ayooluwa Babalola <babalolaayooluwa1@gmail.com>
2023-03-22 18:55:07 -0700

    created main file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,17 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using a given function
- * @name: the name to print
- * @f: the function to use to print the name
+ * print_name - Prints a name
+ * @name: The name to print
+ * @f: A function that takes a char* argument and returns void
  *
- * Return: void
+ * Description: This function prints the specified name
  */
 
-void print_name(char *name, void (*f)(char))
+void print_name(char *name, void (*f)(char *))
 {
-	while (*name != '\0')
+	if (name != NULL && f != NULL)
 	{
-		f(*name);
-		name++;
+		f(name);
 	}
 }
